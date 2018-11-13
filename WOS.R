db41ded91d4cf32f2023a5834dcb13017a319f9f
Dylan <dylangomes@u.boisestate.edu>
2018-11-13 15:16:53 -0700

    Add Ecology

--- WOS.R
+++ WOS.R
@@ -15,6 +15,8 @@
 
 library(dplyr)
 
+### AB
+
 filenames <- dir(path="AB_1918_2018", all.files = TRUE, recursive=TRUE)
 
 
@@ -54,12 +56,53 @@ summary(m1)
 abline(m1, lwd=3)
 
 
-### Check to see that loop worked
-#   df1<-read.delim(paste0("AB_1918_2018/",filenames[1]), header=TRUE, sep="\t", row.names=NULL)
-#   df2<-read.delim(paste0("AB_1918_2018/",filenames[2]), header=TRUE, sep="\t", row.names=NULL)
-#   df3<-read.delim(paste0("AB_1918_2018/",filenames[3]), header=TRUE, sep="\t", row.names=NULL)
-# 
-# df4<-rbind(df1, df2[-1,])
-# df5<-rbind(df4, df3[-1,])
-# 
-# identical(df5,out)
+### Ecology
+filenames <- dir(path="EC_1918_2018", all.files = TRUE, recursive=TRUE)
+
+
+out <- read.delim(paste0("EC_1918_2018/",filenames[1]), header=TRUE, row.names=NULL, quote = "")
+
+for(i in 2:length(filenames)){
+  print(filenames[i])
+  df<-read.delim(paste0("EC_1918_2018/",filenames[i]), header=TRUE, sep="\t", row.names=NULL, quote = "") ### Read each file one at a time in for loop
+  
+  out <- rbind(out, df)
+}
+
+EC<-select(out, PT, Z2, Z3, MA, BP, SU, PD)  ### Gets rid of excess columns that are unecessary 
+
+colnames(EC)<-c("AU", "TI", "SO","BP", "EP", "DATE", "YR")
+
+View(EC)
+
+sort(unique(EC$YR))
+length(unique(EC$YR))
+
+str(EC)
+
+EC$BP<-as.integer(as.character(EC$BP))
+EC$EP<-as.integer(as.character(EC$EP))
+EC$Page<-EC$EP-EC$BP
+
+sort(unique(EC$Page))
+
+EC<-EC[which(EC$Page>0),]
+
+
+plot((EC$Page)~EC$YR, main="Ecology: slope=0.007 p<0.001", ylEC="Page Numbers", xlab="Year")
+
+m1<-lm(EC$Page~EC$YR)
+summary(m1)
+abline(m1, lwd=3)
+
+
+
+## Check to see that loop worked
+  df1<-read.delim(paste0("EC_1918_2018/",filenames[1]), header=TRUE, sep="\t", row.names=NULL, quote = "")
+  df2<-read.delim(paste0("EC_1918_2018/",filenames[2]), header=TRUE, sep="\t", row.names=NULL)
+  df3<-read.delim(paste0("EC_1918_2018/",filenames[3]), header=TRUE, sep="\t", row.names=NULL,quote = "")
+
+df4<-rbind(df1, df2)
+df5<-rbind(df4, df3)
+View(df5)
+identical(df5,out)
